96c9fba86592f1776b100b92432c9878a377a409
Vlad <grinkivvlad2@gmail.com>
2021-07-08 17:19:39 +0300

    add func easter_egg, doc_of_function changed hello to header

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import importlib
 import sys
-import this
-import random
 from django.core.management import execute_from_command_line
 from django.conf import settings
 from django.http import HttpResponse
@@ -15,31 +13,34 @@ DEBUG = True,
 SECRET_KEY = 'secret'
 )
 
-
-list_of_modules = [i for i in dir(importlib.import_module(name='random')) if not i.startswith('_')]
-string_of_modules = '\n'.join([f'<a href="random/{i}">{i}</a><br>' for i in list_of_modules])
-
-
-
-html_file = f"""
+html_file = """
 <!DOCTYPE html>
 <html>
 <head>
 <title>Nebroni</title>
 </head>
 <body>
-{string_of_modules}
+{string}
 <h2>@nebroni</h2>
 </body>
 </html>
 """
 
-def hello(_):
-	return HttpResponse(html_file)
+def easter_egg(_):
+	return HttpResponse('You Found Easter Egg congratulation!!!')
+
+def header(_, name_of_module):
+	list_of_modules = [i for i in dir(importlib.import_module(name=f'{name_of_module}')) if not i.startswith('_')]
+	string_of_modules = '\n'.join([f'<a href="{name_of_module}/{i}">{i}</a><br>' for i in list_of_modules])
+	return HttpResponse(html_file.format(string = string_of_modules))
+
+def doc_of_function(_, name_of_module, name):
+	return HttpResponse(getattr(importlib.import_module(name=f'{name_of_module}'), name).__doc__)
 
 urlpatterns = [
-
-	path('random', hello)
+	path('doc', easter_egg),
+	path('doc/<name_of_module>', header),
+	path('doc/<name_of_module>/<name>', doc_of_function)
 ]
 
 execute_from_command_line(sys.argv)
--- zen.py
+++
@@ -1,8 +0,0 @@
-import importlib
-import random
-ans = []
-print(random.choice.__doc__)
-print(getattr(random, 'random'))
-print(dir(importlib.import_module(name='random')))
-list_of_modules = [i for i in dir(importlib.import_module(name='random')) if not i.startswith('_')]
-string_of_modules = '\n'.join([f'<a href="random/{i}">{i}</a>' for i in list_of_modules])
