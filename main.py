da0eccc3cb98d1f45137836c471ee17e883d8af3
Vlad <grinkivvlad2@gmail.com>
2021-07-08 17:32:17 +0300

    final version

--- main.py
+++ main.py
@@ -1,17 +1,12 @@
 import importlib
 import sys
 from django.core.management import execute_from_command_line
-from django.conf import settings
 from django.http import HttpResponse
 from django.urls import path
 
-
-
-settings.configure(
-ROOT_URLCONF = __name__,
-DEBUG = True,
+ROOT_URLCONF = __name__
+DEBUG = True
 SECRET_KEY = 'secret'
-)
 
 html_file = """
 <!DOCTYPE html>
@@ -26,16 +21,23 @@ html_file = """
 </html>
 """
 
+
 def easter_egg(_):
 	return HttpResponse('You Found Easter Egg congratulation!!!')
 
+
 def header(_, name_of_module):
-	list_of_modules = [i for i in dir(importlib.import_module(name=f'{name_of_module}')) if not i.startswith('_')]
-	string_of_modules = '\n'.join([f'<a href="{name_of_module}/{i}">{i}</a><br>' for i in list_of_modules])
-	return HttpResponse(html_file.format(string = string_of_modules))
+	try:
+		list_of_modules = [i for i in dir(importlib.import_module(name=f'{name_of_module}')) if not i.startswith('_')]
+		string_of_modules = '\n'.join([f'<a href="{name_of_module}/{i}">{i}</a><br>' for i in list_of_modules])
+		return HttpResponse(html_file.format(string=string_of_modules))
+	except ModuleNotFoundError:
+		return HttpResponse(f"No module named '{name_of_module}'")
+
 
 def doc_of_function(_, name_of_module, name):
 	return HttpResponse(getattr(importlib.import_module(name=f'{name_of_module}'), name).__doc__)
+
 
 urlpatterns = [
 	path('doc', easter_egg),
@@ -44,4 +46,3 @@ urlpatterns = [
 ]
 
 execute_from_command_line(sys.argv)
-
