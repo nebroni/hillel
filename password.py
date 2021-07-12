46c0275dd8aaf12db36f5ced0955b22c2b2ed758
Vlad <grinkivvlad2@gmail.com>
2021-07-12 19:04:39 +0300

    delete settings

--- password.py
+++ password.py
@@ -1,8 +1,9 @@
 import string
 import random
+
+
 def password():
 	password = random.sample(
 		[random.choice(string.digits), random.choice(string.ascii_lowercase), random.choice(string.ascii_uppercase)], 3)
 	password += random.sample(string.ascii_letters + string.digits, 2)
 	return ''.join(password)
-
