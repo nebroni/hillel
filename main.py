5ff9c827cef3a8cf49a9fc725f7b5682049c2880
Vlad <grinkivvlad2@gmail.com>
2021-07-12 18:40:28 +0300

    a

--- main.py
+++ main.py
@@ -6,29 +6,19 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.urls import path
 settings.configure(
-	ROOT_URLCONF = __name__,
-	DEBUG = True,
-	SECRET_KEY = 'secret',
-	TAMPLATES = [
+	ROOT_URLCONF=__name__,
+	DEBUG=True,
+	SECRET_KEY='secret',
+	TEMPLATES=[
 		{
-
+			'BACKEND': 'django.template.backends.django.DjangoTemplates',
+			'DIRS': ['C:\\Users\\User\\PycharmProjects\\hillel1\\html'],
 		}
 	]
 )
-html_file = """
-<!DOCTYPE html>
-<html>
-<head>
-<title>Nebroni</title>
-</head>
-<body>
-{string}
-<h2>@nebroni</h2>
-</body>
-</html>
-"""
 
 
+# 1
 def easter_egg(_):
 	return HttpResponse('You Found Easter Egg congratulation!!!')
 
@@ -36,7 +26,7 @@ def easter_egg(_):
 def header(request, name_of_module):
 	try:
 		list_of_modules = [i for i in dir(importlib.import_module(name=f'{name_of_module}')) if not i.startswith('_')]
-		return render(request, 'index.html', {'string':list_of_modules}) #HttpResponse(html_file.format(string=string_of_modules))
+		return render(request, 'index.html', {'string': list_of_modules})
 	except ModuleNotFoundError:
 		return HttpResponse(f"No module named '{name_of_module}'")
 
@@ -44,8 +34,13 @@ def header(request, name_of_module):
 def doc_of_function(_, name_of_module, name):
 	return HttpResponse(getattr(importlib.import_module(name=f'{name_of_module}'), name).__doc__)
 
+# 2
+
+
 
 urlpatterns = [
+	path(''),
+	path('<key>'),
 	path('doc', easter_egg),
 	path('doc/<name_of_module>', header),
 	path('doc/<name_of_module>/<name>', doc_of_function)
